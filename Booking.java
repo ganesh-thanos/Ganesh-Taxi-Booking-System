a923398b4ac7d06411f67b941180ac5d75ed78db
ganesh-thanos <ganesamoorthimca80@gmail.com>
2025-11-04 16:46:21 +0530

    taxi application

---
+++ Booking.java
@@ -0,0 +1,18 @@
+class Booking{
+    int bookingId,customerId,pickUpTime,dropTime,amount;
+    char from,to;
+
+
+    public Booking(){
+        super();
+    }
+    
+    public Booking(int bookingId,int customerId,int pickUpTime,int dropTime,int amount){
+        super();
+        this.bookingId = bookingId;
+        this.customerId = customerId;
+        this.pickUpTime = pickUpTime;
+        this.dropTime = dropTime;
+        this.amount = amount;
+    }
+}
